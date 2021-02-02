d3c3543ff687668b7eac56edf41ddf05adc59a4f
CallumHewitt <CallumHewitt@users.noreply.github.com>
2021-02-02 09:52:35 +0000

    Removing unused functions from new_server.py

--- server_scripts/new_server.py
+++ server_scripts/new_server.py
@@ -1,8 +1,5 @@
-from fabric import Connection, Config
 from server_util import *
 import yaml
-import logging
-import sys
 
 def setup_server():    
     config = get_config()
@@ -33,16 +30,6 @@ def setup_server():
 def get_config():
     with open("config.yml", "r") as stream:
         return yaml.safe_load(stream)
-
-def configure_logging():
-    logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    logging.getLogger('paramiko').setLevel(logging.INFO)
-
-def create_ssh_client(hostname, ssh_key_location):
-    if (ssh_key_location.endswith('ppk')):
-        raise ValueError('.ppk ssh keys are not supported. Please convert to OpenSSH.')
-    client = Connection(hostname,  user='root', connect_kwargs={'key_filename': ssh_key_location})
-    return client
 
 if __name__ == '__main__':
     setup_server()
